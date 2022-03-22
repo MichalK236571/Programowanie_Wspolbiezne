7c8c9d82335cae72572f40f90fdc538e8719e4e0
236578 Kunka-Wagner Robert <236578@edu.p.lodz.pl>
2022-03-22 20:55:22 +0100

    Stage 0 - Example program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Współbieżne
+{
+    public class Program
+    {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Program dodaje dwie liczby");
+            Console.WriteLine("Podaj pierwsza liczbe:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe:");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wynik: " + Add(x, y));
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1Tests/ProgramTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Współbieżne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Współbieżne.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            int x = 5;
+            int y = 12;
+           
+            Assert.AreEqual(Współbieżne.Program.Add(x, y), 17);
+            Assert.AreNotEqual(Współbieżne.Program.Add(x, y), 15);
+        }
+    }
+}
