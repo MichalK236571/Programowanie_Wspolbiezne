cf1638e01967c66093a51d728342760f2f8630e5
236571 Krawczyk Michał <236571@edu.p.lodz.pl>
2022-04-18 13:13:40 +0200

    Class Circle was added

---
+++ Projekt- etap1/ClassLibrary1/Circle.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassLibrary1
+{
+    public class Circle
+    {
+        public int Radious { get; set; }
+        public int XValue { get; set; }
+        public int YValue { get; set; }
+        public int XDirection { get; set; }
+        public int YDirection { get; set; }
+
+        public Circle(int r, int x, int y, int xDirection, int yDirection)
+        {
+            Radious = r;
+            XValue = x;
+            YValue = y;
+            XDirection = xDirection;
+            YDirection = yDirection;
+
+        }
+
+
+
+    }
+}
