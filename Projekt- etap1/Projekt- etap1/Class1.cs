855f8c100c2f05867545bb7bb30fbfefe65fd1d5
236571 Krawczyk Michał <236571@edu.p.lodz.pl>
2022-04-18 14:28:20 +0200

    Basic implementation of Screen class

--- Projekt- etap1/Projekt- etap1/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Projekt__etap1
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Projekt- etap1/Projekt- etap1/Ekran.cs
@@ -0,0 +1,16 @@
+﻿namespace Projekt__etap1
+{
+    public class Ekran
+    {
+        private int width { get; set; }
+        private int height { get; set; }
+
+        public Ekran(int w, int h)
+        {
+            width = w;  
+            height = h;
+        }
+
+
+    }
+}
