c8de6fdc06ba0646adb83edd74c9e786b9c87422
236578 Kunka-Wagner Robert <236578@edu.p.lodz.pl>
2022-04-20 13:20:03 +0200

    Basic View

---
+++ Projekt- etap1/Model/ModelClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Model
+{
+   
+    public class ModelClass
+    {
+
+    }
+}
---
+++ Projekt- etap1/ViewModel/VMClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    public class VMClass 
+    {
+       
+    }
+}
