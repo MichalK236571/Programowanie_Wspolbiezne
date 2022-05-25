5c489bf1477aa6f5469506266b1f522a9d219562
236578 Kunka-Wagner Robert <236578@edu.p.lodz.pl>
2022-05-25 16:07:34 +0200

    Model Refactoring

---
+++ Project-stage1/Presentation/Model/BallModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Logic;
+
+namespace Presentation.Model
+{
+    internal class BallModel : BallModelAPI, INotifyPropertyChanged
+    {
+        private int x;
+        private int y;
+        private int radius;
+
+        public override int X
+        {
+            get => x;
+            set
+            {
+                x = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override int Y
+        {
+            get => y;
+            set
+            {
+                y = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override int Radius
+        {
+            get => radius;
+            set => radius = value;
+        }
+
+        public override event PropertyChangedEventHandler? PropertyChanged;
+
+        public BallModel(int x, int y, int radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public override void UpdateCircle(Object s, PropertyChangedEventArgs e)
+        {
+            BallLogicAPI ball = (BallLogicAPI)s;
+            if (e.PropertyName == "XValue")
+            {
+                X = ball.XValue;
+            }
+            else if (e.PropertyName == "YValue")
+            {
+                Y = ball.YValue;
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Project-stage1/Presentation/Model/BallModelAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Presentation.Model
+{
+    public abstract class BallModelAPI
+    {
+        public static BallModelAPI CreateCircle(int xPosition, int yPosition, int radius)
+        {
+            return new BallModel(xPosition, yPosition, radius);
+        }
+
+        public abstract event PropertyChangedEventHandler? PropertyChanged;
+        public abstract int X { get; set; }
+
+        public abstract int Y { get; set; }
+
+        public abstract int Radius { get; set; }
+
+        public abstract void UpdateCircle(Object s, PropertyChangedEventArgs e);
+    }
+}
