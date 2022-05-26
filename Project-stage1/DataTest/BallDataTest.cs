2ba59abf260923378d29c73eb70d9c8c444b371a
236571 Krawczyk Michał <236571@edu.p.lodz.pl>
2022-05-26 19:01:53 +0200

    Tests for DataLayer

---
+++ Project-stage1/DataTest/BallDataTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data;
+
+namespace DataTest
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void Getter_Setter_Test()
+        {
+            BallDataAPI ballDataAPI = BallDataAPI.CreateBallDataAPI(10,10,15,50,5,5);
+            Assert.AreEqual(15, ballDataAPI.Radius);
+            Assert.AreEqual(50, ballDataAPI.Weight);
+            Assert.AreEqual(5, ballDataAPI.XDirection);
+            Assert.AreEqual(5, ballDataAPI.YDirection);
+
+            ballDataAPI.XValue = 15;
+            ballDataAPI.YValue = 15;
+            Assert.AreEqual(15, ballDataAPI.XValue);
+            Assert.AreEqual(15, ballDataAPI.YValue);
+        }
+
+    }
+}
---
+++ Project-stage1/DataTest/BoardTest.cs
@@ -0,0 +1,28 @@
+using Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataTest
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void CreateBall_Test()
+        {
+            BoardAPI boardAPI = BoardAPI.createAPI(20, 20);
+            boardAPI.createDataBallAPI(10, 10, 10, 10);
+            Assert.AreEqual(1,boardAPI.getBalls().Count);
+        }
+
+        [TestMethod]
+        public void removeBalls_Test()
+        {
+            BoardAPI boardAPI = BoardAPI.createAPI(20, 20);
+            boardAPI.createDataBallAPI(10, 10, 10, 10);
+            boardAPI.createDataBallAPI(10, 10, 10, 10);
+            Assert.AreEqual(2, boardAPI.getBalls().Count);
+            boardAPI.removeBalls();
+            Assert.AreEqual(0, boardAPI.getBalls().Count);
+        }
+    }
+}
