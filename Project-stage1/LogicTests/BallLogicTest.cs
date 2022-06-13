d69dcb0061cef1ed4ae180edd7919bef8fb6edfd
236571 Krawczyk Michał <236571@edu.p.lodz.pl>
2022-06-13 14:33:38 +0200

    Tests  for LogicBall and LogicBallManager

--- Project-stage1/LogicTests/BallLogicTest.cs
+++ Project-stage1/LogicTests/BallLogicTest.cs
@@ -5,27 +5,28 @@ using Logic;
 
 namespace LogicTests
 {
-  /*  [TestClass]
+    [TestClass]
     public class BallLogicTest
     {
         [TestMethod]
         public void CreateBall_Test()
         {
-            BallLogicAPI ball = BallLogicAPI.CreateBall(10, 10, 15, 10, 2, 2);
-            Assert.AreEqual(10, ball.XValue);
+            BallLogicAPI ball = BallLogicAPI.CreateBall(10, 10, 15);
             Assert.AreEqual(10, ball.YValue);
+            Assert.AreEqual(10, ball.XValue);
             Assert.AreEqual(15, ball.Radius);
-            Assert.AreEqual(10, ball.Weight);
-            Assert.AreEqual(2,ball.XDirection);
-            Assert.AreEqual(2,ball.YDirection);
+
+
         }
 
         [TestMethod]
         public void Setter_Test()
         {
-            BallLogicAPI ball = BallLogicAPI.CreateBall(10, 10, 15, 10, 2, 2);
+            BallLogicAPI ball = BallLogicAPI.CreateBall(10, 10, 15);
             ball.XValue = 15;
-            Assert.AreEqual(15, ball.XValue);  
+            ball.YValue = 12;
+            Assert.AreEqual(15, ball.XValue);
+            Assert.AreEqual(12, ball.YValue);
         }
-    }*/
+    }
 }
--- Project-stage1/LogicTests/BallsManagerTest.cs
+++ Project-stage1/LogicTests/BallsManagerTest.cs
@@ -3,7 +3,7 @@ using Logic;
 
 namespace LogicTests
 {
-    /*[TestClass]
+    [TestClass]
     public class BallsManagerTest
     {
         [TestMethod]
@@ -11,9 +11,23 @@ namespace LogicTests
         {
             LogicAbstactAPI ballManager = LogicAbstactAPI.CreateApi(150, 150);
             ballManager.CreateBallInRandomPlace();
-            Assert.AreEqual(1,ballManager.GetAllBalls().Count);
+            Assert.AreEqual(1, ballManager.GetAllBalls().Count);
             ballManager.RemoveAllBalls();
-            Assert.AreEqual(0,ballManager.GetAllBalls().Count); 
+            Assert.AreEqual(0, ballManager.GetAllBalls().Count);
         }
-    }*/
+
+        [TestMethod]
+        public void MakeBall_Test()
+        {
+            LogicAbstactAPI ballManager = LogicAbstactAPI.CreateApi(150, 150);
+            BallLogicAPI ball = ballManager.MakeBall(15, 15, 5, 5);
+            Assert.AreEqual(15, ball.XValue);
+            Assert.AreEqual(15, ball.YValue);
+
+        }
+    }
+
+
+
+
 }
